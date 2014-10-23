bdaf86a00b8b7147edfdd6e9017bc714a43146dc
Hua Ma <hua.ma@infusionsoft.com>
2014-10-23 16:13:31 -0700

    added toolbar.

--- app/src/main/java/net/sf/appcompv21/MainActivity.java
+++ app/src/main/java/net/sf/appcompv21/MainActivity.java
@@ -2,15 +2,32 @@ package net.sf.appcompv21;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_ab_drawer);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+
     }
 
     @Override
@@ -20,16 +37,13 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    //
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(Gravity.START);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
